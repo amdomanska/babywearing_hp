08fe7300eaf03b9c95335b48537f20e36b6cb338
Aga Domanska <domanska.a.m.87@gmail.com>
2018-11-20 11:16:31 +0000

    add image menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,32 @@ import props from './conf'
 
 import Banner from './Components/Banner'
 import AboutMe from './Components/AboutMe'
+import Menu from './Components/Menu'
 
 class App extends Component {
   render() {
     return (
-      <Tab.Container defaultActiveKey="first">
-        <Row>
-          <Col sm={12}>
-            <Nav bsStyle="tabs" className='myTabs'>
-              <NavItem eventKey="first" className='myTab'>Menu</NavItem>
-              <NavItem eventKey="second" className='myTab'>About Me</NavItem>
-              <NavItem eventKey="logo" className='logo'>Agnieszka Mitas-Syrek</NavItem>
-            </Nav>
-          </Col>
-          <Col sm={12}>
-            <Tab.Content animation>
-              <Tab.Pane eventKey="first"></Tab.Pane>
-              <Tab.Pane eventKey="second"><AboutMe {...props} /></Tab.Pane>
-              <Tab.Pane eventKey="logo"><Banner {...props} /></Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
+      <div>
+        <Tab.Container defaultActiveKey="first">
+          <Row>
+            <Col sm={12}>
+              <Nav bsStyle="tabs" className='myTabs'>
+                <NavItem eventKey="first" className='myTab'>Menu</NavItem>
+                <NavItem eventKey="second" className='myTab'>About Me</NavItem>
+                <NavItem eventKey="logo" className='logo'>Agnieszka Mitas-Syrek</NavItem>
+              </Nav>
+            </Col>
+            <Col sm={12}>
+              <Tab.Content animation>
+                <Tab.Pane eventKey="first"></Tab.Pane>
+                <Tab.Pane eventKey="second"><AboutMe {...props} /></Tab.Pane>
+                <Tab.Pane eventKey="logo"><Banner {...props} /></Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+        <Menu />
+      </div>
     )
 }
 }
--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Modal, Button } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 
-import horizontal1 from '../../images/bg1transp.svg';
-import horizontal2 from '../../images/bg2transp.svg';
-import horizontal3 from '../../images/bg3transp.svg';
-
-import AboutMe from '../AboutMe'
-
+import horizontal1 from '../../images/horizontal1small.jpg';
+import horizontal2 from '../../images/horizontal2small.jpg';
+import horizontal3 from '../../images/horizontal3small.jpg';
 
 import './Menu.css'
 
@@ -16,67 +13,104 @@ export default class Menu extends Component {
 
   constructor(props) {
     super(props)
+    this.interval = null;
     this.state = {
-      showAboutMeModal: false
+      why: true,
+      shop: false,
+      workshops: false
     }
   }
-  render() {
-    console.log(this.props)
 
+  componentWillMount() {
+    this.interval = setInterval(() => {
+      if (this.state.why) {
+        this.setState({why: false, shop: true, workshops: false})
+      }
+      else if (this.state.shop) {
+        this.setState({why: false, shop: false, workshops: true})
+      }
+      else if (this.state.workshops) {
+        this.setState({why: true, shop: false, workshops: false})
+      }
+    }, 3000);
+  }
+
+  handleClickRight = () => {
+    clearInterval(this.interval)
+    if (this.state.why) {
+      this.setState({why: false, shop: true, workshops: false})
+    }
+    else if (this.state.shop) {
+      this.setState({why: false, shop: false, workshops: true})
+    }
+    else if (this.state.workshops) {
+      this.setState({why: true, shop: false, workshops: false})
+    }
+    console.log(this.state)
+  }
+
+  handleClickLeft = () => {
+    clearInterval(this.interval)
+    if (this.state.why) {
+      this.setState({why: false, shop: false, workshops: true})
+    }
+    else if (this.state.shop) {
+      this.setState({why: true, shop: false, workshops: false})
+    }
+    else if (this.state.workshops) {
+      this.setState({why: false, shop: true, workshops: false})
+    }
+    console.log(this.state)
+  }
+
+  render() {
     return (
       <div>
-      <Grid className='Component'>
-        <Row className='myRow'>
-          <Col md={4} 
-            style={{
-              backgroundImage: `url(${horizontal1})`, 
-              backgroundSize: 'contain', 
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center'
-              }}
-              onClick={() => this.setState({showAboutMeModal: true})}
-            className='menuItem'
-          >
-            <div className='title'>About me</div>
-          </Col>
-          <Col md={4} style={{
-              backgroundImage: `url(${horizontal2})`, 
-              backgroundSize: 'contain', 
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              }}
-              className='menuItem'>
-            <div className='title'>Advantages</div>
-          </Col>
-          <Col md={4} style={{
-              backgroundImage: `url(${horizontal3})`, 
-              backgroundSize: 'contain', 
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center'
-              }}
-              className='menuItem'>
-            <div className='title'>Online shop</div>
-          </Col>
-        </Row>
-      </Grid>
-      
-      <Modal 
-        show={this.state.showAboutMeModal} 
-        onHide={() => this.setState({showAboutMeModal: false})}
-        dialogClassName="custom-modal"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <h3>{this.props.aboutMe.title}</h3>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body style={{paddingTop: '10%', paddingBottom: '10%'}}>
-          <AboutMe {...this.props} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => this.setState({showAboutMeModal: false})}>Close</Button>
-        </Modal.Footer>
-      </Modal>
+        <div className='slider' id='slider'>
+          <div className='holder'>
+            <div className='slide' id='slide-0'>
+              { this.state.why && 
+                <a href='/why'>
+                  <Image src={horizontal1}
+                    className='pics'
+                  />
+                  <div className='linkText'>
+                    <h1>WHY TO BABYWEAR?</h1>
+                    <h5>Why should I bother?</h5>
+                  </div>
+                </a>
+              }
+            </div>
+            <div className='slide' id='slide-1'>
+              { this.state.shop && 
+                <a href='/shop'>
+                  <Image src={horizontal2}
+                    className='pics'
+                  />
+                  <div className='linkText'>
+                    <h1>SHOP</h1>
+                    <h5>What can I buy?</h5>
+                  </div>
+                </a>
+              }
+            </div>
+            <div className='slide' id='slide-2'>
+              { this.state.workshops && 
+                <a href='/workshop'>
+                  <Image src={horizontal3}
+                    className='pics'
+                  />
+                  <div className='linkText'>
+                    <h1>WORKSHOP</h1>
+                    <h5>Where and when I can learn it?</h5>
+                  </div>
+                </a>
+              }
+            </div>
+          </div>
+        </div>
+        <i class="fas fa-angle-double-right fa-3x right" onClick={this.handleClickRight}></i>
+        <i class="fas fa-angle-double-left fa-3x left"  onClick={this.handleClickLeft}></i>
       </div>
     );
   }
